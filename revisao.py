15e050f8c3f254f2dd94001d69199c83edea977a
Gabriella630 <mariagabriellaamarante630@gmail.com>
2025-09-05 13:59:10 +0000

    revisao de lista

---
+++ revisao.py
@@ -0,0 +1,36 @@
+'''frutas = ['uva', 'banana', 'melancia', 'morango', 'melao']
+
+print(frutas[0])
+print(frutas[4])
+print(frutas)'''
+
+numeros = [1,2,3,4,5]
+
+numeros.append(6)
+print(numeros)
+
+numeros.insert(0,0)
+print(numeros)
+
+numeros.remove(3)
+print(numeros)
+
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+
+num = lista1 + lista2
+print(num)
+
+letras = ['b', 'd', 'a', 'c', 'e']
+
+letras.sort()
+print(letras)
+
+letras.reverse()
+print(letras)
+
+print(letras.count('a'))
+print(letras)
+
+letras.pop()
+print(letras)
